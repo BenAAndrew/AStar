d8836891908dd47f4a4c95e31811606ab592592b
BenAAndrew <bandrew01@qub.ac.uk>
2019-12-26 18:44:08 +0000

    Add docstrings

--- graphics/window.py
+++ graphics/window.py
@@ -3,12 +3,29 @@ from sys import exit
 
 
 class Window:
-    def __init__(self, width, height, title):
+    """
+    Window class for rendering the maze solving.
+    Wraps the pygame screen with a simple update method.
+    """
+
+    def __init__(self, width: int, height: int, title: str):
+        """
+        Initialises pygame and the screen
+        
+        Arguments:
+            width {int} -- screen width in pixels
+            height {int} -- screen height in pixels
+            title {str} -- window title
+        """
         pygame.init()
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption(title)
 
     def update(self):
+        """
+        Updates the pygame window or exits if the window
+        is closed
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
