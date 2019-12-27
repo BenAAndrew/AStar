96c0aab088f01491cc3e3da4b7c3c69ccd906d0e
BenAAndrew <bandrew01@qub.ac.uk>
2019-12-27 15:43:17 +0000

    Add docstrings

--- tools/vector.py
+++ tools/vector.py
@@ -1,4 +1,9 @@
 class Vector:
+    """
+    Class to represent a position on the maze.
+    Stores x & y and enables some useful tools.
+    """
+
     def __init__(self, tuple):
         self.x = tuple[0]
         self.y = tuple[1]
@@ -13,4 +18,14 @@ class Vector:
         return self.x == vector.x and self.y == vector.y
 
     def distance_to(self, vector):
+        """
+        Gets the x+y distance between itself and
+        another vector
+        
+        Arguments:
+            vector {Vector} -- Vector to measure distance to
+        
+        Returns:
+            {int} -- total distance between vectors
+        """
         return int(abs(self.x - vector.x) + abs(self.y - vector.y))
