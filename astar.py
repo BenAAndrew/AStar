35a29e79dec4fed4c71d93bac4fc8184678f3801
BenAAndrew <bandrew01@qub.ac.uk>
2019-12-27 15:45:46 +0000

    Add docstring

--- astar.py
+++ astar.py
@@ -1,7 +1,6 @@
 from time import sleep
 
 from graphics.window import Window
-from astar_logic.maze import Maze
 from astar_logic.decision_handler import DecisionHandler
 from tools.load_maze import load_maze
 from tools.load_arguments import load_arguments, get_value, get_command_line_arg
@@ -18,6 +17,13 @@ decision_handler = DecisionHandler(MAZE)
 
 # Run process
 while True:
+    """
+    Main running process.
+    Updates the screen & renders the grid.
+    If the maze is still being solved it also 
+    updates decision_handler to execute the 
+    next step in the solve.
+    """
     screen.update()
     decision_handler.maze.grid.draw(screen)
     if decision_handler.continue_solving:
